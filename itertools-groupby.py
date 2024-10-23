01339b07d9ab005c4dcec9c59c158c5d00230d53
Nethra Harini M <nethraharini0904@gmail.com>
2024-10-24 02:12:51 +0530

    itertools-groupby.py

---
+++ itertools-groupby.py
@@ -0,0 +1,19 @@
+#using inbuilt function groupby we separate key and input and prints them, using join we finally join the len of list group and key,
+#and we call the function in main function
+from itertools import groupby
+
+def compress_string(s):
+    grouped = groupby(s)
+    result = ' '.join(f"({len(list(group))}, {key})" for key, group in grouped)
+    return result
+
+if __name__ == '__main__':
+    input_string = input().strip()  
+    output = compress_string(input_string)  
+    print(output) 
+
+#input
+#1222311
+#ouput
+#(1, 1) (3, 2) (1, 3) (2, 1)
+
