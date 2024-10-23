8347f04bc7c95a3f011f3bb03de6926c09eb1b5d
Nethra Harini M <nethraharini0904@gmail.com>
2024-10-24 02:19:03 +0530

    itertools-combinations_with_replacements.py

---
+++ itertools-combinations_with_replacements.py
@@ -0,0 +1,28 @@
+#
+
+from itertools import combinations_with_replacement
+data = (input().strip())
+s,n = data.split()
+n = int(n)
+combinations_result = combinations_with_replacement(sorted(s),n)
+for combo in combinations_result:
+    print(''.join(combo))
+
+
+
+#input:
+#HACK 2
+
+#output:
+#AA
+#AC
+#AH
+#AK
+#CC
+#CH
+#CK
+#HH
+#HK
+#KK
+
+
