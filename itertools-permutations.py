8cd5915a07e42f5e95d5337e95b1975fcbb84b8d
Nethra Harini M <nethraharini0904@gmail.com>
2024-10-25 20:42:19 +0530

    itertools-permutations.py

---
+++ itertools-permutations.py
@@ -0,0 +1,25 @@
+from itertools import permutations
+user_input = (input())
+string_part, number_part = user_input.split()
+number_part= int(number_part)
+sorted_string = ''.join(sorted(string_part))
+permutated_string = list(permutations(sorted_string,number_part))
+for perm in permutated_string:
+    print(''.join(perm))
+
+
+#input
+#HACK 2
+#output
+#AC
+#AH
+#AK
+#CA
+#CH
+#CK
+#HA
+#HC
+#HK
+#KA
+#KC
+#KH
