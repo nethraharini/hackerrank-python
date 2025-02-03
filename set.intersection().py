1fe081f5dc1d4f965ba965c863668e6537873044
Nethra Harini M <nethraharini0904@gmail.com>
2025-02-03 20:06:28 +0530

    Create set.intersection().py

---
+++ set.intersection().py
@@ -0,0 +1,21 @@
+n = int(input())
+english = set(input().split())
+b = int(input())
+french = input().split()
+
+print(len(english.intersection(french)))
+
+
+
+
+
+
+"""
+Input (stdin)
+9
+1 2 3 4 5 6 7 8 9
+9
+10 1 2 3 11 21 55 6 8
+Expected Output
+5
+"""
